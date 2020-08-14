ebdfc5c4201920ddeeba7cec2754a2af2e023fa4
varostud <69037907+varostud@users.noreply.github.com>
2020-08-14 17:33:40 +0300

    Add files via upload

---
+++ easy-collection/MoveZeroes/MoveZeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zeros = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            nums[i - zeros] = nums[i];
+            if(nums[i] == 0)
+            {
+                zeros++;
+            }
+        }
+        for(int i = nums.size() - zeros; i < nums.size(); i++)
+        {
+            nums[i] = 0;
+        }
+    }
+};
