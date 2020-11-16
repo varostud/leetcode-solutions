b9b22ab0324f7dc7acc7f81c57846db39df06556
varostud <varostud0@gmail.com>
2020-11-17 01:39:52 +0300

    remove mistake

--- leetcode-solutions/easy-collection/Array/ContainsDuplicate/ContainsDuplicate.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:   
-    bool containsDuplicate(vector<int>& nums) {
-        set<int> temp(nums.begin(), nums.end());
-        return temp.size() != nums.size();
-    }
-};
